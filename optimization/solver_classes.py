c30c5c95c0bf4ba9445658f88656e83bae6366b3
sobibence <sobibence@gmail.com>
2025-03-13 15:22:09 +0100

    removed connecting node from UI

--- optimization/solver_classes.py
+++ optimization/solver_classes.py
@@ -1,7 +1,8 @@
 import cvxpy as cp
 import math
 import numpy as np
-
+import abc
+import inspect
 
 class BaseSolverClass():
     pass
@@ -47,20 +48,31 @@ class Node():
         self.problem_class = problem_class
         problem_class.add_node(self)
         self.name = ""
-
+        
+    # @abc.abstractmethod
     def getConstraints(self):
         return []
+    
+    @abc.abstractmethod
+    def getConnectingNode(self):
+        return
+    
+    @abc.abstractmethod
+    def setConnectingNode(self, connecting_node):
+        return
     
     def getVariables(self):
         return []
     
+    @abc.abstractmethod
     def getCost(self):
-        return 0
+        return
     
 class ConnectingNode(Node):
     def __init__(self, problem_class):
         super().__init__(problem_class)
         self.connected_nodes = []
+        self.name = "ConnectingNode"
 
     def connect(self, node):
         self.connected_nodes.append(node)
@@ -73,58 +85,103 @@ class ConnectingNode(Node):
     
 ## find a better name for this class, because it is not a leaf     
 class DeviceNode(Node):
-    def __call__(self, problem_class):
-        return super().__init__(problem_class)
-    
+    def __init__(self, problem_class):
+        super().__init__(problem_class)
+        self.connecting_node = None
+
+    @abc.abstractmethod
     def getPowerFlow(self, connecting_node : ConnectingNode):
-        return 0
+        return
     
+    @abc.abstractmethod
     def getCost(self):
-        return 0
+        return 
     
     def getVariables(self):
         return []
+    
+    def getConnectingNode(self):
+        return self.connecting_node
+    
+    def setConnectingNode(self, connecting_node):
+        self.connecting_node = connecting_node
+    
+    def __sub__(self, other: Node): 
+        if self.connecting_node is None and other.getConnectingNode() is None:
+            print("Connecting nodes are None: " + self.name)
+            self.connecting_node = ConnectingNode(self.problem_class)
+            other.setConnectingNode(self.connecting_node)
+            self.connecting_node.connect(self)
+            self.connecting_node.connect(other)
+        elif self.connecting_node is None:
+            print("Connecting node is None: " + self.name)
+            self.connecting_node = other.getConnectingNode()
+            self.connecting_node.connect(self)
+        elif other.connecting_node is None:
+            print("Other connecting node is None: " + self.name)
+            other.setConnectingNode(self.connecting_node)
+            self.connecting_node.connect(other)  
+            
+    # define rsub as doing nothing
+    # def __rsub__(self, other: Node):
+    #     return self
         
 class TransmissionLine(DeviceNode):
-    def __init__(self, problem_class, connecting_node_1 : ConnectingNode, connecting_node_2 : ConnectingNode, capacity, transmission_loss = 0):
+    def __init__(self, problem_class, capacity, transmission_loss = 0):
         super().__init__(problem_class)
-        # direction is 1 -> 2
-        self.connecting_node_1 = connecting_node_1
-        self.connecting_node_2 = connecting_node_2
-        connecting_node_1.connect(self)
-        connecting_node_2.connect(self)
+        # direction is left(1) -> right (2)
+        # special case as it will have 2 connecting nodes. The left one will be the standard connecting node
+        # right one will be connecting_node_right
+        self.connecting_node_right = None
         self.transmission_loss = transmission_loss
         self.capacity = capacity  
         self.name = "TransmissionLine"
+        
+    def __sub__(self, other: Node):
+        if self.connecting_node_right is None and other.getConnectingNode() is None:
+            self.connecting_node_right = ConnectingNode(self.problem_class)
+            other.setConnectingNode(self.connecting_node_right)
+            self.connecting_node_right.connect(self)
+            self.connecting_node_right.connect(other)
+        elif self.connecting_node_right is None:
+            self.connecting_node_right = other.getConnectingNode()
+            self.connecting_node_right.connect(self)
+        elif other.connecting_node is None:
+            other.setConnectingNode(self.connecting_node_right)
+            self.connecting_node_right.connect(other)
+            
+    def getConnectingNode(self):
+        return self.connecting_node_right
+    
+    def setConnectingNode(self, connecting_node):
+        self.connecting_node_right = connecting_node
 
     def setTimeLen(self, time_len):
         self.time_len = time_len
-        self.power_flow_1_2 = cp.Variable(time_len)
-        self.power_flow_2_1 = cp.Variable(time_len)
+        self.power_flow_left_right = cp.Variable(time_len)
+        self.power_flow_right_left = cp.Variable(time_len)
 
     def getConstraints(self, t):
         return [
-            self.power_flow_1_2[t] <= self.capacity,
-            self.power_flow_2_1[t] <= self.capacity
+            self.power_flow_left_right[t] <= self.capacity,
+            self.power_flow_right_left[t] <= self.capacity
         ] 
     
     def getPowerFlow(self, connecting_node : ConnectingNode, t):
-        if connecting_node == self.connecting_node_1:
-            return (1-self.transmission_loss)*self.power_flow_2_1[t] - self.power_flow_1_2[t]
-        elif connecting_node == self.connecting_node_2:
-            return (1-self.transmission_loss)*self.power_flow_1_2[t] - self.power_flow_2_1[t]
+        if connecting_node == self.connecting_node_right:
+            return (1-self.transmission_loss)*self.power_flow_left_right[t] - self.power_flow_right_left[t]
+        elif connecting_node == self.connecting_node:
+            return (1-self.transmission_loss)*self.power_flow_right_left[t] - self.power_flow_left_right[t]
         else:
             return 0
         
     def getVariables(self):
-        return [self.power_flow_1_2 - self.power_flow_2_1]
+        return [self.power_flow_left_right - self.power_flow_right_left]
 
 
 class Producer(DeviceNode):
-    def __init__(self, problem_class, connecting_node: ConnectingNode, production_capacity, price = 10):
+    def __init__(self, problem_class, production_capacity, price = 10):
         super().__init__(problem_class)
-        self.connecting_node = connecting_node
-        connecting_node.connect(self)
         self.production_capacity = production_capacity
         self.price = price
         self.name = "Producer"
@@ -147,10 +204,8 @@ class Producer(DeviceNode):
         return [self.production_schedule]
     
 class Consumer(DeviceNode):
-    def __init__(self, problem_class, connecting_node: ConnectingNode):
+    def __init__(self, problem_class):
         super().__init__(problem_class)
-        self.connecting_node = connecting_node
-        connecting_node.connect(self)
         self.name = "Consumer"
 
     def setTimeLen(self, time_len):
@@ -166,17 +221,15 @@ class Consumer(DeviceNode):
         return -self.consumption_schedule[t]
 
 class Prosumer(DeviceNode):
-    def __init__(self, problem_class, connecting_node: ConnectingNode, production_capacity, consumption_capacity):
+    def __init__(self, problem_class, production_capacity, consumption_capacity):
         super().__init__(problem_class)
-        self.connecting_node = connecting_node
-        connecting_node.connect(self)
         self.production_capacity = production_capacity
         self.consumption_capacity = consumption_capacity
         
 
 class PowerExchange(Prosumer):
-    def __init__(self, problem_class, connecting_node: ConnectingNode, production_capacity, consumption_capacity):
-        super().__init__(problem_class, connecting_node, production_capacity, consumption_capacity)
+    def __init__(self, problem_class, production_capacity, consumption_capacity):
+        super().__init__(problem_class, production_capacity, consumption_capacity)
         self.name = "PowerExchange"
 
     def setTimeLen(self, time_len):
@@ -198,8 +251,8 @@ class PowerExchange(Prosumer):
         return [self.powerFlow]
     
 class Battery(Prosumer):
-    def __init__(self, problem_class, connecting_node: ConnectingNode, production_capacity, consumption_capacity, battery_capacity, efficiency = 0.9):
-        super().__init__(problem_class, connecting_node, production_capacity, consumption_capacity)
+    def __init__(self, problem_class, production_capacity, consumption_capacity, battery_capacity, efficiency = 0.9):
+        super().__init__(problem_class, production_capacity, consumption_capacity)
         self.battery_capacity = battery_capacity
         self.efficiency = efficiency
         self.name = "Battery"
@@ -237,13 +290,17 @@ class Battery(Prosumer):
     
 if __name__ == "__main__":
     problemClass = GraphProblemClass()
-    node1 = ConnectingNode(problemClass)
-    node2 = ConnectingNode(problemClass)
-    producer = Producer(problemClass, node1, production_capacity=5, price=2)
-    consumer = Consumer(problemClass, node2)
-    battery = Battery(problemClass, node2, production_capacity= 5, consumption_capacity= 5, battery_capacity= 5)
-    transmission_line = TransmissionLine(problemClass, node1, node2, capacity=5, transmission_loss=0)
-    power_exchange = PowerExchange(problemClass, node1, production_capacity= 10, consumption_capacity=5)
+    producer = Producer(problemClass, production_capacity=5, price=2)
+    consumer = Consumer(problemClass)
+    battery = Battery(problemClass, production_capacity= 5, consumption_capacity= 5, battery_capacity= 5)
+    transmission_line = TransmissionLine(problemClass, capacity=5, transmission_loss=0)
+    power_exchange = PowerExchange(problemClass, production_capacity= 10, consumption_capacity=5)
+
+    producer - transmission_line
+    transmission_line - consumer
+    producer - power_exchange
+    battery - consumer
+    
 
     problemClass.setTimeLen(5)    
     power_exchange.setPrices([1,2,4,5,1])
@@ -252,5 +309,13 @@ if __name__ == "__main__":
     problemClass.solve()
     problemClass.printResults()
     
+    # print(inspect.signature(producer))
+    print(inspect.signature(Producer))
+    print(inspect.signature(Producer.getPowerFlow))
+    print(inspect.signature(producer.getPowerFlow))
+    # print(inspect.signature(producer))
+    print(inspect.getsource(Producer))
+    print(inspect.getsource(Producer.getPowerFlow))
+    print(inspect.getsource(producer.getPowerFlow))
 
     
