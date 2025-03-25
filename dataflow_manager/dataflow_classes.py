e1c0a17317fdef52c53a722ef5c84425dfbaf312
sobibence <sobibence@gmail.com>
2025-03-25 16:50:19 +0100

    basic dependency dataflow

--- dataflow_manager/dataflow_classes.py
+++ dataflow_manager/dataflow_classes.py
@@ -3,7 +3,7 @@ from typing import Dict
 import pandas as pd
 import uuid
 import requests
-import dataflow_manager.dataflow_manager as manager
+# import dataflow_manager.dataflow_manager as manager
 
 db_connection = None  # Placeholder for the database connection
 
@@ -11,7 +11,7 @@ class DataflowNode:
     def __init__(self, name: str) -> None:
         self.id = uuid.uuid4()
         self.name = name
-        manager.addNode(self)
+        # manager.addNode(self)
 
     def execute(self, dfs: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
         raise NotImplementedError("Subclasses should implement this method")
@@ -78,11 +78,70 @@ class DataFetchingFromAPINode(DataflowNode):
         dfs[self.name] = pd.DataFrame(response.json())
         return dfs
 
+class DependencyNode():
+    _dependencies = []
+    _is_executed = False
+    _results = {}
+    
+    def __init__(self, name: str):
+        self._name = name
 
+    def add_dependency(self, node):
+        self._dependencies.append(node)
+    
+    # overlode >> operator
+    def __rrshift__(self, node):
+        self.add_dependency(node)
+        return self
+    
+    def __rshift__(self, node):
+        return self
+    
+    def getResults(self) -> Dict[str, pd.DataFrame]:
+        return self._results
+    
+    def run(self):
+        if not self._is_executed:
+            for node in self._dependencies:
+                node.run()
+            input_dfs = {}
+            for node in self._dependencies:
+                input_dfs = input_dfs | node.getResults()
+            self._results = self.process(input_dfs)
+            self._is_executed = True
+    
+    def process(self, dfs: Dict[str, pd.DataFrame]) -> Dict[str, pd.DataFrame]:
+        return dfs      
+    
+                
+    
+    
 # Example usage
 if __name__ == "__main__":
     dfs = {"initial_df": pd.DataFrame(
         {"column1": [1, 2, 3], "column2": [4, 5, 6]})}
-    data_node = DataProcessingNode("data_node", "sample_data")
-    result_dfs = data_node.execute(dfs)
-    print(result_dfs)
+    dfs2 = {"initial_df2": pd.DataFrame(
+        {"column1": [10, 20, 30], "column2": [40, 50, 60]})}
+    dep1 = DependencyNode("csv1")
+    dep2 = DependencyNode("csv2")
+    dep3 = DependencyNode("preproc")
+    dep4 = DependencyNode("model")
+    
+    dep1 >> dep3
+    dep2 >> dep3
+    dep3 >> dep4
+    
+    dep1.process = lambda dfs: {"csv1": pd.DataFrame({"column1": [1, 2, 3], "column2": [4, 5, 6]})}
+    dep2.process = lambda dfs: {"csv2": pd.DataFrame({"column1": [10, 20, 30], "column2": [40, 50, 60]})}
+    # merge incoming dataframes
+    dep3.process = lambda dfs: {"preproc": pd.concat([dfs["csv1"], dfs["csv2"]])}
+    # double all values
+    dep4.process = lambda dfs: {"model": dfs["preproc"] * 2}
+    
+    dep4.run()
+    
+    print(dep4.getResults())
+    
+    # data_node = DataProcessingNode("data_node", "sample_data")
+    # result_dfs = data_node.execute(dfs)
+    # print(result_dfs)
